1e4976df5bcedb1c32e2dc53e1517d05011771bb
Your Name <you@example.com>
2025-01-14 14:58:38 +0600

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import About from "./pages/About"
 import Profile from "./pages/Profile"
+import Header from "./components/Header"
 
 
 
@@ -11,6 +12,7 @@ import Profile from "./pages/Profile"
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
